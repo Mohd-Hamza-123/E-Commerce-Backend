4cfab1dd8a80684795d919ae7c7e70c5743ae1be
Mohd Hamza <mohdhamza.me@gmail.com>
2024-10-24 19:57:12 +0530

    configuring cors

--- src/helpers/filter-truthy-values.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.filterTruthyValues = void 0;
-const filterTruthyValues = (Obj) => {
-    return Object.fromEntries(Object.entries(Obj).filter(([key, value]) => (value)));
-};
-exports.filterTruthyValues = filterTruthyValues;
--- src/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_middleware_1 = require("../middlewares/auth-middleware");
-const router = (0, express_1.Router)();
-router.post(`place-order`, auth_middleware_1.checkUserAuthorized);
-exports.default = router;
